39cf5abe1971bb3c6c59073cc634c4ea1e2bf5ec
Vladislav Brezgun <nextydev@gmail.com>
2023-06-27 12:31:29 +0300

    Add project files.

---
+++ src/Services/Identity/Identity.API/Program.cs
@@ -0,0 +1,14 @@
+namespace LibraLibrium.Services.Identity.API;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+        var app = builder.Build();
+
+        app.MapGet("/", () => "Hello World!");
+
+        app.Run();
+    }
+}
