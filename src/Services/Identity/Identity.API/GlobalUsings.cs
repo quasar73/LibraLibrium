aee895d84852830efcca0d8de17232adf894a0be
Vladislav Brezgun <nextydev@gmail.com>
2023-06-29 17:52:58 +0300

    added seq, renamed solution

--- src/Services/Identity/Identity.API/GlobalUsings.cs
+++ src/Services/Identity/Identity.API/GlobalUsings.cs
@@ -1,1 +1,8 @@
-﻿
+﻿global using Microsoft.AspNetCore;
+global using Microsoft.AspNetCore.Hosting;
+global using Microsoft.AspNetCore.Server.Kestrel.Core;
+global using Microsoft.Extensions.Hosting;
+global using Serilog;
+global using System;
+global using System.Net;
+
---
+++ src/Services/Identity/Identity.API/Startup.cs
@@ -0,0 +1,19 @@
+﻿namespace LibraLibrium.Services.Identity.API;
+
+public class Startup
+{
+    public IConfiguration Configuration { get; }
+
+    public Startup(IConfiguration configuration)
+    {
+        Configuration = configuration;
+    }
+
+    public void ConfigureServices(IServiceCollection services)
+    {
+    }
+
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ILoggerFactory loggerFactory)
+    {
+    }
+}
